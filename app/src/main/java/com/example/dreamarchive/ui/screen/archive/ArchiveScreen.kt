9dc0d6a6eec084f1f3e18319cca4df9520c27ef1
mio_osato <sugar_sugar2000@icloud.com>
2024-09-02 23:59:02 +0900

    他の遷移のための用意

--- app/src/main/java/com/example/dreamarchive/ui/screen/archive/ArchiveScreen.kt
+++ app/src/main/java/com/example/dreamarchive/ui/screen/archive/ArchiveScreen.kt
@@ -1,2 +1,50 @@
 package com.example.dreamarchive.ui.screen.archive
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ArchiveScreen(
+    navController: NavController
+){
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        "Archive"
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navController.navigate("settingscreen") }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = "SettingDrawer"
+                        )
+                    }
+                }
+            )
+        },
+    ) {paddingValues ->
+        Column (
+            modifier = Modifier.padding(paddingValues)
+        ){
+            Text(text = "This is the archive screen.")
+        }
+    }
+}
