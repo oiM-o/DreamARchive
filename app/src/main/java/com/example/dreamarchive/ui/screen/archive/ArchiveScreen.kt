4c800a1fc0cee8ba535092247aa10cfd0f003ca3
araund2001 <qqyw6tr9k@joy.ocn.ne.jp>
2024-09-17 19:07:51 +0900

    SettingとMyAchiveの画面を整えました

--- app/src/main/java/com/example/dreamarchive/ui/screen/archive/ArchiveScreen.kt
+++ app/src/main/java/com/example/dreamarchive/ui/screen/archive/ArchiveScreen.kt
@@ -14,25 +14,37 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.dreamarchive.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ArchiveScreen(
     navController: NavController
 ){
+
+    //紫系統のカラーを定義
+    val darkPurple = Color(0xFF6A1B9A)
+    val lightPurple = Color(0xFFCE93D8)
+    val mediumPurple = Color(0xFF8E24AA)
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
                     Text(
-                        "Archive"
+                        "Archive",
+                        color = Color.White
                     )
                 },
                 navigationIcon = {
@@ -41,41 +53,52 @@ fun ArchiveScreen(
                     ) {
                         Icon(
                             imageVector = Icons.Default.Settings,
-                            contentDescription = "SettingDrawer"
+                            contentDescription = "SettingDrawer",
+                            tint = Color.White
                         )
                     }
-                }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = darkPurple
+                )
             )
         },
         bottomBar = {
-            NavigationBar {
-                Row(
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    IconButton(
+            NavigationBar (
+                containerColor = darkPurple
+            ) {
+                Row{
+                    NavigationBarItem(
                         onClick = { navController.navigate("talkscreen") },
-                        modifier = Modifier.weight(1f)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Edit,
-                            contentDescription = "Edit"
-                        )
+                        modifier = Modifier.weight(1f),
+                        icon = {
+                            Icon(imageVector = Icons.Default.Edit,
+                                contentDescription = "Edit",
+                                tint = Color.White
+                            )
+                        },
+                        label = { Text("Edit", color = Color.White) },
+                        selected = false
+                    )
 
-                    }
-
-                    IconButton(
+                    NavigationBarItem(
                         onClick = { navController.navigate("archivescreen") },
-                        modifier = Modifier.weight(1f)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Star,
-                            contentDescription = "Star"
-                        )
-                    }
+                        modifier = Modifier.weight(1f),
+                        icon = {
+                            // drawable フォルダにあるリソースを呼び出し
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_import_contacts_24),
+                                contentDescription = "MyARchive",
+                                tint = Color.White
+                            )
+                        },
+                        label = { Text("MyARchive", color = Color.White) },
+                        selected = false
+                    )
 
                 }
             }
-        }
+        },
     ) {paddingValues ->
         Column (
             modifier = Modifier.padding(paddingValues)
